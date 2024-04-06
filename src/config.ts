2df32328420baec4664b53b073dacf3c64ae4988
revrenlove <58348549+revrenlove@users.noreply.github.com>
2024-04-06 16:08:56 -0500

    Moving static config values to `config.json` (#145)
    Dynamically determining cursor position for new C# items.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
-import * as vscode from 'vscode';
+import * as configJson from './config.json';
 
 export class Config {
-    public static readonly genericTemplatePath = `${__dirname}/templates/itemFile.tmpl`;
-    public static readonly genericTemplateCursorPosition = new vscode.Position(6, 8);
+    public static readonly genericTemplatePath = `${__dirname}/${configJson.genericTemplatePath}`;
 }
